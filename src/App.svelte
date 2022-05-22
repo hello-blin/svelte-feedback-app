e8ced69696a52631077e341d1fd27e29cd8c5fb3
Blin Kukaj <kblin164@gmail.com>
2022-05-22 23:35:25 +0200

    Working on Higher level Components.

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,13 @@
 <script>
 	import FeedbackList from "./Components/FeedbackList.svelte";
-	// export const FeedbackStore = writable([
-	// 	{
-	// 		id: 1,
-	// 		rating: 10,
-	// 		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-	// 	},
-	// 	{
-	// 		id: 2,
-	// 		rating: 9,
-	// 		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-	// 	},
-	// 	{
-	// 		id: 3,
-	// 		rating: 8,
-	// 		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-	// 	},
-	// ]);
+	import feedback from "./Components/FeedbackList.svelte";
 </script>
 
 <main>
 	<FeedbackList />
 </main>
+
+
+<style>
+	
+</style>
--- src/Components/FeedbackList.svelte
+++ src/Components/FeedbackList.svelte
@@ -1,4 +1,26 @@
 <script>
+    import FeedbackItem from "./FeedbackItem.svelte";
+    export const feedback = [
+        {
+            id: 1,
+            rating: 10,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+        },
+        {
+            id: 2,
+            rating: 9,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+        },
+        {
+            id: 3,
+            rating: 8,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+        },
+    ];
 </script>
 
-FeedbackList
+{#each feedback as fb (fb.id)}
+    <h3>
+        <FeedbackItem item={fb} />
+    </h3>
+{/each}
